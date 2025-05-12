dddb7b03ded98b1cdd5a71ed198672837d6b8e86
Santos Perazzolo <88857260+SanBurGos@users.noreply.github.com>
2025-05-12 03:17:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.getElementById("theme-toggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+const toggle = document.getElementById('theme-toggle');
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  toggle.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
 });
